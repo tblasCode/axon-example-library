77234b1a13fe451695872b624bc58d5dbf954755
tblasCode <tomas.deblas@vass.es>
2020-10-18 23:59:28 +0200

    first commit

---
+++ src/main/java/com/jos/lab/axon/library/AxonExampleLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.jos.lab.axon.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonExampleLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonExampleLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jos/lab/axon/library/AxonExampleLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jos.lab.axon.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxonExampleLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
